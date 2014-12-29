b862b7d0d4f9d7c46798753554a318538ee7c191
Vasco fernandes <vasco@myworkout.no>
2014-12-29 17:12:29 +0000

    Tests

---
+++ Customers_VRPF.Tests/Controllers/CustomersControllerTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Customers_VRPF;
+using Customers_VRPF.Controllers;
+using Customers_VRPF.DataTransferObjects;
+using Customers_VRPF.Models;
+
+namespace Customers_VRPF.Tests.Controllers
+{
+    [TestClass]
+    public class CustomersControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            var controller = new CustomersController();
+
+            // Act
+            var actionResult = controller.Get();
+            var response = actionResult as OkNegotiatedContentResult<IQueryable<CustomerDto>>;
+            var response2 = response.Content.ToArray();
+            // Assert
+            Assert.IsNotNull(response2);
+            Assert.AreEqual(5, response2.Count());
+            Assert.AreEqual("Mette", response2.ElementAt(0).Name);
+            Assert.AreEqual("Vasco", response2.ElementAt(1).Name);
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            var controller = new CustomersController();
+
+            // Act
+            var actionResult = controller.Get(1);
+            var response = actionResult as OkNegotiatedContentResult<CustomerDto>;
+            // Assert
+            Assert.AreEqual("Mette", response.Content.Name);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            // Act
+            controller.Post(new Customer {Name = "Test1", Address = "Reidulvs gate 5", City = "Trondheim", Country = "Norge" });
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            // Act
+            controller.Put(5, new Customer { Id = 5, Name = "Test2", Address = "Reidulvs gate 5", City = "Trondheim", Country = "Norge" });
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            // Act
+            controller.Delete(5);
+
+            // Assert
+        }
+    }
+}
--- Customers_VRPF/Controllers/CustomersController.cs
+++ Customers_VRPF/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net;
 using System.Web.Http;
 using Customers_VRPF.Interfaces;
@@ -57,123 +58,46 @@ namespace Customers_VRPF.Controllers
 
         public IHttpActionResult Post([FromBody]Customer customer)
         {
-            var newCustomer = _unit.Customers.Add(customer);
-            // _unit.Commit(); // Would do work if we had a DB
+            try
+            {
+                var newCustomer = _unit.Customers.Add(customer);
+                // _unit.Commit(); // We dont have a DB yet
 
-            var url = Url.Link("DefaultApi", new { controller = "Customers", id = newCustomer.Id });
+                var url = Url.Link("DefaultApi", new { controller = "Customers", id = newCustomer.Id });
 
-            return Created(url, newCustomer);
+                return Created(url, newCustomer);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("We need to hadle this!", e);
+                return InternalServerError();
+            }
         }
 
         //update customer
         public IHttpActionResult Put(int id, [FromBody]Customer updatedCustomer)
         {
-
-            var customer = _unit.Customers.Update(id, updatedCustomer);
-            // _unit.Commit(); // Would do work if we had a DB
-
+            try
+            {
+                var customer = _unit.Customers.Update(id, updatedCustomer);
+                // _unit.Commit(); // We dont have a DB yet
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("We need to hadle this!", e);
+                return InternalServerError();
+            }
+            
             return Ok("Ok");
         }
 
         public IHttpActionResult Delete(int id)
         {
             _unit.Customers.Remove(id);
-            _unit.Commit(); // Would do work if we had a DB
+            // _unit.Commit(); // We dont have a DB yet
 
             return StatusCode(HttpStatusCode.NoContent);
         }
-
-
-
-
-        /*
-        InventoryEntities db = new InventoryEntities();
-
-        //get all customer
-        [HttpGet]
-        public IEnumerable<Customer> Get()
-        {
-            return db.Customers.AsEnumerable();
-        }
-
-        //get customer by id
-        public Customer Get(int id)
-        {
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
-            {
-                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
-            }
-            return customer;
-        }
-
-        //insert customer
-        public HttpResponseMessage Post(Customer customer)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Customers.Add(customer);
-                db.SaveChanges();
-                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, customer);
-                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = customer.Id }));
-                return response;
-            }
-            else
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-            }
-        }
-
-        //update customer
-        public HttpResponseMessage Put(int id, Customer customer)
-        {
-            if (!ModelState.IsValid)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-            }
-            if (id != customer.Id)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest);
-            }
-            db.Entry(customer).State = EntityState.Modified;
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
-            }
-            return Request.CreateResponse(HttpStatusCode.OK);
-        }
-
-        //delete customer by id
-        public HttpResponseMessage Delete(int id)
-        {
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.NotFound);
-            }
-            db.Customers.Remove(customer);
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
-            }
-            return Request.CreateResponse(HttpStatusCode.OK, customer);
-        }
-
-        //prevent memory leak
-        protected override void Dispose(bool disposing)
-        {
-            db.Dispose();
-            base.Dispose(disposing);
-        }
-        */
 
     }
 }
