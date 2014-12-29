977ecfa0f4da84de2f7c21cf5d836e333d776149
Vasco fernandes <vasco@myworkout.no>
2014-12-29 13:32:12 +0000

    add app.js

---
+++ Customers_VRPF/app/app.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("customers", ["ui.router", "services"]);
+
+    angular.module("customers").config(["$stateProvider", "$urlRouterProvider",
+        function ($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise("/customers");
+
+            $stateProvider
+            .state("customersList", {
+                url: "/customers",
+                templateUrl: "app/customers/customersListView.html",
+                controller: "CustomersListCtrl as vm"
+            })
+            .state("customersDetail", {
+                url: "/customers/:customerId/view",
+                templateUrl: "app/customers/customersDetailView.html",
+                controller: "CustomersDetailCtrl as vm",
+                resolve: {
+                    customersResource: "customersResource",
+               
+                    customer: function (customersResource, $stateParams) {
+                        var customerId = $stateParams.customerId;
+                        // Forcing resolution before the controller executes with $promise
+                        return customersResource.get({ customerId: customerId }).$promise; 
+                    }
+                }
+            })
+            .state("customersEdit", {
+                url: "/customers/:customerId/edit",
+                templateUrl: "app/customers/customersEditView.html",
+                controller: "CustomersEditCtrl as vm",
+                resolve: {
+                    customersResource: "customersResource",
+
+                    customer: function (customersResource, $stateParams) {
+                        var customerId = $stateParams.customerId;
+                        // Forcing resolution before the controller executes with $promise
+                        return customersResource.get({ customerId: customerId }).$promise;
+                    }
+                }
+            });
+        }
+    ]);
+
+})();
